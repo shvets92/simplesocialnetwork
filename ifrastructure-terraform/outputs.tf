4de3d92647cdce85bc8496761d3107ec862e3b9f
Anthony Shvets <anthonyshvets@gmail.com>
2019-08-13 17:09:07 -0500

    Delete outputs.tf

--- ifrastructure-terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "DBAddress" {
-  value = aws_db_instance.simple_social_database.address
-}
-
-output "S3BucketDomainName" {
-  value = aws_s3_bucket.simple_social_storage.bucket_domain_name
-}
-
